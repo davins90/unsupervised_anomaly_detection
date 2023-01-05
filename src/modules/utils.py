11829b5f29a61acb1305676b5929035b77a1dead
dani <daniele.davino@live.it>
2023-01-05 23:09:12 +0100

    finish anomaly, ongoing clustering

---
+++ src/modules/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import gdown
+
+def data_retrieval(url):
+    """
+    
+    """
+    file_id=url.split('/')[-2]
+    dwn_url='https://drive.google.com/uc?id=' + file_id
+    print(dwn_url)
+    if '1pCcXTt5OURpAoo0JCuDqFrQt3vo_xmT3' in url:
+        df = pd.read_csv(dwn_url)
+    else:
+        output = "../../data_lake/input/transaction_raw.csv"
+        gdown.download(dwn_url,output,quiet=False)
+        # remove from dataframe columns related to Vesta's generated features
+        df = pd.read_csv(output,usecols=lambda x: "V" not in x)
+    return df
